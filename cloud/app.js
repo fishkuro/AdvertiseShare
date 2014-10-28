c225e2a578bdf749447b11bc37b564e086329780
fishkuro <17905184@163.com>
2014-10-28 11:41:04 +0800

    confirm

--- cloud/app.js
+++ cloud/app.js
@@ -205,12 +205,12 @@ app.get('/administrator/tasksdata',function(req, res) {
   res.json(tasksdata);
 });
 
-app.get('/administrator/tasksdatatest',function(req, res) {
-  var tasks = AV.Object.extend("Tasks");
-  var query = new AV.Query(tasks);
-  var tasksdata = query.find();
-  res.json(tasksdata);
-});
+// app.get('/administrator/tasksdatatest',function(req, res) {
+//   var tasks = AV.Object.extend("Tasks");
+//   var query = new AV.Query(tasks);
+//   var result = query.find();
+//   res.json(result);
+// });
 
 app.get('/administrator/terracesdata',function(req, res) {
   var terracesdata = TerracesCls.find();

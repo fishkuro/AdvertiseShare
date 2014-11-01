ca612cff3059fa55c854d51bd8f9926e1497348f
fishkuro <Kurodo@KurodotekiMac-mini.local>
2014-11-01 15:30:17 +0800

    terrace.id

--- cloud/app.js
+++ cloud/app.js
@@ -306,9 +306,9 @@ app.get('/administrator/tasksdata',function(req, res) {
     {
       var terraceArr = new Array();
       for (var i = data.length - 1; i >= 0; i--) {
-        var terrace = data[i].get("parent");
+        var terrace = data[i].get("Terraces");
         //terraceArr[i] = terrace.objectId;
-        console.log("get tasksdata 1: " + terrace.objectId);
+        console.log("get tasksdata 1: " + terrace.id);
       }
 
       // var Terraces = TerracesCls.query();
